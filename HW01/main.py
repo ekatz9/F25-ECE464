8c0319b60caf613851e78df87c9275d722e375ec
ekatz9 <78486787+ekatz9@users.noreply.github.com>
2025-09-02 15:52:20 -0500

    Verbose Comments, TODO

--- HW01/main.py
+++ HW01/main.py
@@ -8,40 +8,52 @@ with open('input.txt','r') as file:
     # for line in lines:
     #     print(line)
 
-# categorize inputs & outputs & gates
+    # Data structures for output
     inputs = []
     outputs = []
     gates = []
 
-    # parse each line into
-    # 'nodeOut' = 'nType'('nodeIn')
     for line in lines:
+
+        # consider each input line conforms to the format
+        # 'nodeOut' = 'nType'('nodeIn')
 
         # find delimiter indices
         equal = line.find('=')
         open = line.find('(')
         close = line.find(')')
+        
+        # DEBUG: print line from input as reference
         print(line)
+
+        # DEBUG: check delimiter indices
         # print(f'{line} -> {equal},{open},{close}')
         
+        # Find appropriate Substrings
         nodeOut = line[0:equal].strip()
         nodeIn = line[open+1:close].strip()
 
-        if equal > 1:
+        # input/output declarations do not include '='
+        # find() returns -1 if character not found
+        if equal > 0:
             nType = line[equal+1:open].strip()
             # node is a logic gate
             print(f'{nodeOut} is an {nType} Gate')
+            # TODO: add to data structure
+        
         else:
             nType = line[0:open].strip()
             if nType == 'INPUT':
                 #node is an input
                 print(f'{nodeIn} is an input')
+                # TODO: add to data structure
+
             if nType == 'OUTPUT':
                 #node is an output
                 print(f'{nodeIn} is an output')
+                # TODO: add to data structure
 
-    for gate in gates:
-        print(gate)
+    # TODO: format output
 
     # Barbaric input sensing
     #for line in lines:
