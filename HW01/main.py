772316b24a50962c6eb548e375c698725275e850
Mazin Al-Attrash <123397430+ShockWave707@users.noreply.github.com>
2025-09-05 16:14:06 -0500

    Update main.py

--- HW01/main.py
+++ HW01/main.py
@@ -22,43 +22,67 @@ with open('input.txt','r') as file:
         equal = line.find('=')
         open = line.find('(')
         close = line.find(')')
-        
-        # DEBUG: print line from input as reference
-        print(line)
-
-        # DEBUG: check delimiter indices
-        # print(f'{line} -> {equal},{open},{close}')
-        
-        # Find appropriate Substrings
-        nodeOut = line[0:equal].strip()
-        nodeIn = line[open+1:close].strip()
 
         # input/output declarations do not include '='
         # find() returns -1 if character not found
         if equal > 0:
             nType = line[equal+1:open].strip()
             # node is a logic gate
-            print(f'{nodeOut} is an {nType} Gate')
-            # TODO: add to data structure
-        
+            # Find appropriate Substrings
+            nodeOut = line[0:equal].strip()
+            nodeIn = line[open+1:close].strip()
+            # Find how many inputs
+            inputCount = 0
+            for i in nodeIn:
+                if i.isalpha():
+                    inputCount += 1
+            
+            #format string to be "nodeOut InputCount GateType nodeIn"
+            gate = nodeOut + ' ' + str(inputCount) + ' ' + nType + ' ' + nodeIn
+            gates.append(gate)
+
+
         else:
             nType = line[0:open].strip()
             if nType == 'INPUT':
                 #node is an input
-                print(f'{nodeIn} is an input')
-                # TODO: add to data structure
+                inputs.append(line[open+1:close].strip())
 
             if nType == 'OUTPUT':
                 #node is an output
-                print(f'{nodeIn} is an output')
-                # TODO: add to data structure
+                outputs.append(line[open+1:close].strip())
 
-    # TODO: format output
+    # Print All Inputs
+    print("\n           ==INPUT==")
+    for i in inputs:
+        print(f'           ||  {i}  ||')
+    print("           =========\n")
 
-    # Barbaric input sensing
-    #for line in lines:
-    #    if line[0:5] == 'INPUT':
-    #        print(f'input {line[6]} detected')
-    #    if line[0:6] == 'OUTPUT':
-    #        print(f'output {line[7]} detected')
+    # Print All Outputs
+    print("          ==OUTPUTS==")
+    for i in outputs:
+        print(f'          ||   {i}   ||')
+    print("          ===========\n")
 
+    # Print All Gates
+    print("=============GATES==============")
+    
+    print("==OUT==# INPUT==TYPE=====IN=====")
+    for i in gates:
+        # Format Gate Data
+        data = i.split()
+        data[0] = data[0].center(3)
+        data[1] = data[1] + "-Input"
+        data[2] = data[2].center(4)
+
+        # Combine All Inputs And Center
+        index = 4
+        s = len(data)
+        while index < s:
+            data[3] += data[index]
+            index += 1
+        data[3] = data[3].center(8)
+
+        print(f'||{data[0]}||{data[1]}||{data[2]}||{data[3]}||')
+    
+    print("================================\n")
