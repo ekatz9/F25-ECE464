d1069520c51e8b9115e2b9f11a6cae1070d44507
ekatz9 <78486787+ekatz9@users.noreply.github.com>
2025-10-18 01:28:06 -0500

    fault not simulated

--- P01/main.py
+++ P01/main.py
@@ -17,10 +17,18 @@ class node:
         self.type = type
         self.gate = gate
         self.value = ''
+        self.fault = ''
         self.nodesIn = nodesIn
-    
+
     def __str__(self):
         return self.value
+    
+class fault:
+    def __init__(self, id, name, type,):
+        self.id = id
+        self.name = name,
+        self.type = type
+        self.detected = False
 
 # Choose input files
 fileList = []
@@ -58,18 +66,18 @@ with open(fileList[fileInd],'r') as file:
 
         # find delimiter indices
         equal = line.find('=')
-        open = line.find('(')
+        openbr = line.find('(')
         close = line.find(')')
 
         # Find appropriate Substrings
         nodeOut = line[0:equal].strip()
-        nodeIn = line[open+1:close].strip()
+        nodeIn = line[openbr+1:close].strip()
 
         # input/output declarations do not include '='
         # find() returns -1 if character not found
         if equal > 0:
             # node is a logic gate
-            nType = line[equal+1:open].strip()
+            nType = line[equal+1:openbr].strip()
 
             # Python silliness to strip inputs for circ[]
             inputs = [t.strip() for t in nodeIn.split(',')]
@@ -82,7 +90,7 @@ with open(fileList[fileInd],'r') as file:
                 circuit[nodeOut] = node(nodeOut, '', nType, inputs)
         
         else:
-            nType = line[0:open].strip()
+            nType = line[0:openbr].strip()
             if nType == 'INPUT':
                 #node is an input
                 cInputs.append(nodeIn)
@@ -116,6 +124,7 @@ for listing in printList:
 #################
 #1) Fault Listing
 
+faultlist = []
 print('\n1) Full Fault List')
 input('   press enter to continue')
 #TODO: populate full fault list
@@ -124,6 +133,10 @@ printList = sorted(circuit.items(), key = lambda item: item[1].name)
 print(f'\n||{' count ':=^10}||{' location ':=^10}||{' fault ':=^10}')
 for listing in printList:
     faults = f'{listing[1].name}-SA0, {listing[1].name}-SA1'
+    
+    faultlist.append(fault(listing[1].name,listing[1].name,'SA0'))
+    faultlist.append(fault(listing[1].name,listing[1].name,'SA1'))
+
     faultcounter += 2
     carriage = 3
     for faultType in {'SA0', 'SA1'}:
@@ -132,6 +145,9 @@ for listing in printList:
                 faults += f'\n||{faultcounter:^10}||{'':^10}||'
                 carriage = 1
             faults += f', {listing[1].name}-{ninput}-{faultType}'
+            #print(f'{listing[1].name} has input {ninput}')
+            faultlist.append(fault(listing[1].name,ninput,faultType))
+
             faultcounter += 1
             carriage += 1
     print(f'||{faultcounter:^10}||{listing[1].name:^10}||{faults}')
@@ -240,7 +256,117 @@ for cOutput in cOutputs:
 for coutput in cOutputs:
     print(f'output {coutput} = {circuit[coutput].value}')
 
-printList = sorted(circuit.items(), key = lambda item: item[1].name)
-print(f'\n||{' id ':=^8}||{' name ':=^10}||{' value ':=^9}||')
-for listing in printList:
-    print(f'||{listing[1].id:^8}||{listing[1].name:^10}||{listing[1].value:^9}||')
+# terminal buffer insufficient, print to output.txt
+with open('output.txt','w') as z:
+    printList = sorted(circuit.items(), key = lambda item: item[1].name)
+    print(f'\n||{' id ':=^8}||{' name ':=^10}||{' value ':=^9}||')
+    print(f'\n||{' id ':=^8}||{' name ':=^10}||{' value ':=^9}||', file = z)
+    for listing in printList:
+        print(f'||{listing[1].id:^8}||{listing[1].name:^10}||{listing[1].value:^9}||')
+        print(f'||{listing[1].id:^8}||{listing[1].name:^10}||{listing[1].value:^9}||', file = z)
+
+#####################
+# 3) Fault Simulation
+
+#function getvalue
+#   take as input Node elem
+#   returns as output value of elem (x,0,1)
+#   reads and writes to circuit
+def getfault(elem,cfault):
+
+    #stupid fault stuff
+    # base case: output fault
+    # is fault at this gate
+    if elem.name[0] == cfault.id:
+        #is fault at outputf
+        if cfault.id == cfault.name[0]:
+            # return fault type
+            if faultType == 'SA0':
+                return '0'
+            else:
+                return '1'
+            
+    # base case: elem already has a value
+    if elem.fault != '':
+        # print(f'   {elem.name} has value {elem.fault}')
+        return elem.fault
+    
+    # base case: elem has 1 input (NOT Gate)
+    if elem.gate == 'NOT':
+        currVal = circuit[elem.nodesIn[0]].fault
+        if currVal == '0':
+            elem.fault = '1'
+        elif currVal == '1':
+            elem.fault = '0'
+        else:
+            elem.fault = 'x'
+        # print(f'   {elem} = !{currVal} = {elem.fault}')
+        return elem.fault
+    
+    # get values of elem inputs
+    inputVals = []
+    # # print(f'   inputs = {elem.nodesIn}')
+    for elemInput in elem.nodesIn:
+        thisval = getfault(circuit[elemInput],cfault)
+        # is fault at this gate
+        if elem.name == cfault.id:
+            # is fault at this input
+            print(f'{elemInput} == {cfault.name[0]}')
+            if elemInput == cfault.name[0]:
+                print(f'ping! {cfault.id}-{cfault.name}-{cfault.type}')
+                # what is fault type
+                if cfault.type == 'SA0':
+                    thisval = '0'
+                else:
+                    thisval = '1'
+        inputVals.append(thisval)
+
+    # print(f'   inputVals = {inputVals}')
+
+    # use ulogic lookup table
+    currVal = inputVals[0]
+    # print(f'   currVal = {currVal}')
+    for i in range(1,len(inputVals)):
+        #format for lookup table
+        vals = sorted({currVal,inputVals[i]})
+        # # print(f'   vals = {vals}')
+        valstring = "".join(vals)
+        match valstring:
+            case '0':
+                currVal = ulogictable[elem.gate][0]
+            case '01':
+                currVal = ulogictable[elem.gate][1]
+            case '0x':
+                currVal = ulogictable[elem.gate][2]
+            case '1':
+                currVal = ulogictable[elem.gate][3]
+            case '1x':
+                currVal = ulogictable[elem.gate][4]
+            case 'x':
+                currVal = ulogictable[elem.gate][5]
+    elem.fault = currVal
+    # print(f'      {elem.name} = {elem.gate}({elem.nodesIn}) = {elem.gate}({inputVals}) = {elem.fault}')
+    return elem.fault
+
+def clearfaults():
+    for elem in circuit:
+        circuit[elem].fault = ''
+
+for cfault in faultlist:
+    #assign starting values
+    for i in range(len(cInputs)):
+        circuit[cInputs[i]].fault = tvector[i]
+
+    for coutput in cOutputs:
+        print(f'{getfault(circuit[coutput],cfault)[0]}, expected {circuit[coutput].value}')
+        if getfault(circuit[coutput],cfault)[0] != circuit[coutput].value:
+            cfault.detected = True
+
+    clearfaults()
+
+for cfault in faultlist:
+    if cfault.detected == True:
+        print(f'{cfault.id}-{cfault.name[0]}-{cfault.type} = {cfault.detected}')
+    
+
+    
